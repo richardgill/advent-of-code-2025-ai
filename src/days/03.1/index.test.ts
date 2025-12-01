8a9df8d9b9edd2b663e99df6151c3f8d745f0a10
Richard Gill <richard@rgill.co.uk>
2025-12-01 13:27:45 +0000

    Update test and solution files for all days

--- src/days/03.1/index.test.ts
+++ src/days/03.1/index.test.ts
@@ -1,7 +1,8 @@
 import { expect, test } from "bun:test";
+import { solve } from "./index";
 
 const example1 = await Bun.file(import.meta.dir + "/data/example1.txt").text();
 
-test("example 1", () => {
-  expect(true).toBe(true);
+test("dummy test", () => {
+  expect(solve(example1)).toBeTruthy();
 });
--- src/days/04.2/index.ts
+++ src/days/04.2/index.ts
@@ -2,7 +2,7 @@ import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
-const solve = (input: string) => {
+export const solve = (input: string) => {
   const data = lines(input);
   return data.length;
 };
