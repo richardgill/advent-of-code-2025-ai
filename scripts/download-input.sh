22fb4e8c8ad1fda46c8702122b3cf363b9bdfb66
Richard Gill <richard@rgill.co.uk>
2025-12-01 12:25:13 +0000

    Update README title and add overwrite flag to download script

--- scripts/download-input.sh
+++ scripts/download-input.sh
@@ -9,7 +9,7 @@ day_padded=$(printf "%02d" "$day")
 mkdir -p "src/days/${day_padded}.1/data"
 mkdir -p "src/days/${day_padded}.2/data"
 
-aoc download -y "$year" -d "$day" --input-only -i "src/days/${day_padded}.1/data/input.txt"
+aoc download -y "$year" -d "$day" --input-only -o -i "src/days/${day_padded}.1/data/input.txt"
 cp "src/days/${day_padded}.1/data/input.txt" "src/days/${day_padded}.2/data/input.txt"
 
 echo "Downloaded input to src/days/${day_padded}.1/data/input.txt and src/days/${day_padded}.2/data/input.txt"
