589538656ff0d1c2bc105bd17721a33d3c8dac62
Richard Gill <richard@rgill.co.uk>
2025-12-03 08:59:23 +0000

    Solve Advent of Code 2025 Day 3

--- src/days/03.1/index.ts
+++ src/days/03.1/index.ts
@@ -2,9 +2,22 @@ import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
+const findMaxJoltage = (bank: string): number => {
+  let maxJoltage = 0;
+
+  for (let i = 0; i < bank.length - 1; i++) {
+    for (let j = i + 1; j < bank.length; j++) {
+      const joltage = Number(bank[i]) * 10 + Number(bank[j]);
+      maxJoltage = Math.max(maxJoltage, joltage);
+    }
+  }
+
+  return maxJoltage;
+};
+
 export const solve = (input: string) => {
-  const data = lines(input);
-  return data.length;
+  const banks = lines(input);
+  return banks.reduce((sum, bank) => sum + findMaxJoltage(bank), 0);
 };
 
 console.log(solve(input));
--- src/days/03.2/index.ts
+++ src/days/03.2/index.ts
@@ -2,9 +2,35 @@ import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
-export const solve = (input: string) => {
-  const data = lines(input);
-  return data.length;
+const TARGET_DIGITS = 12;
+
+const findMaxJoltage = (bank: string): bigint => {
+  let result = "";
+  let startIdx = 0;
+
+  for (let digitsNeeded = TARGET_DIGITS; digitsNeeded > 0; digitsNeeded--) {
+    const endIdx = bank.length - digitsNeeded + 1;
+    let maxDigit = "0";
+    let maxPos = startIdx;
+
+    for (let i = startIdx; i < endIdx; i++) {
+      const digit = bank.charAt(i);
+      if (digit > maxDigit) {
+        maxDigit = digit;
+        maxPos = i;
+      }
+    }
+
+    result += maxDigit;
+    startIdx = maxPos + 1;
+  }
+
+  return BigInt(result);
 };
 
-console.log(solve(input));
+export const solve = (input: string) => {
+  const banks = lines(input);
+  return banks.reduce((sum, bank) => sum + findMaxJoltage(bank), 0n);
+};
+
+console.log(solve(input).toString());
